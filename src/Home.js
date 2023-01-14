e1ebaca73761536226a4655970daaadccc8c5a4a
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-01-14 23:11:58 +0530

    learn outputting list

--- src/Home.js
+++ src/Home.js
@@ -7,23 +7,19 @@ import { useState } from "react";
 const Home = () => {
 
     //use state
-   const [name,setName] =useState('iranga')
-   const [age,setAge]=useState(20) 
-
-
-// this is click events and functions
-const handleClick = () =>{
-    setName('amal');
-    setAge(21)
-
-}
-
+  const [blogs,setblogs] =useState([
+    {title:"my new website " , body: "lorem ipsum..." ,author : "mario" , id:1 },
+    {title:"Welcome party " , body: "lorem ipsum..." ,author : "mario" , id:2 },
+    {title:"Web dev top tips " , body: "lorem ipsum..." ,author : "mario" , id:3 }
+  ])
     return ( 
-        <div className="Content">
-            <h2> Homepage</h2>
-            <p> My name is {name} . i am {age} years old; </p>
-           <button onClick={handleClick}>click me</button>
-        
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <h2>{blog.author}</h2>
+                </div>
+            ))}
         </div>
      );
 }
