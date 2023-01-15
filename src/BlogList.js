c02f37880b690784e47a2cf3f0fe40abf1f83404
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-01-15 16:11:13 +0530

    function as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,13 @@
 
 
 const BlogList = (props) => {
+
    const blogs =props.blogs;
     const title =props.title;
+    const handelDelete =props.handelDelete;
 
-   console.log(props,blogs)
+    
+
 
     return ( 
         <div className="blog-list">
@@ -12,7 +15,8 @@ const BlogList = (props) => {
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h1>{blog.title}</h1>
-                    <h2>{blog.author}</h2>
+                    <h2> Written by {blog.author}</h2>
+                    <button onClick={()=>handelDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,22 @@ const Home = () => {
     //use state
     const [blogs,setblogs] =useState([
       {title:"my new website " , body: "lorem ipsum..." ,author : "mario" , id:1 },
-      {title:"Welcome party " , body: "lorem ipsum..." ,author : "mario" , id:2 },
+      {title:"Welcome party " , body: "lorem ipsum..." ,author : "yoshi" , id:2 },
       {title:"Web dev top tips " , body: "lorem ipsum..." ,author : "mario" , id:3 }
     ])
 
+    const handelDelete = (id) =>{
+
+      const newBlog =blogs.filter(blog => blog.id !== id);
+      setblogs(newBlog);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs}  title="All blogs" />
+            <BlogList blogs={blogs}  title="All blogs" handelDelete={handelDelete} />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario') } title="MArio's blogs" />
+
+
         </div>
      );
 }
